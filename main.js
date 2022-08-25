74668c7ea999389c0804fedc3031926e2362691d
= <anwar988805@yahoo.com>
2022-08-25 21:57:44 +0000

    Update timeout to 2 secs

--- main.js
+++ main.js
@@ -1,29 +1,3 @@
-// const btnSubmit = document.getElementById('btnSubmit')
-
-// btnSubmit.addEventListener('click', (e) => {
-//   e.preventDefault()
-
-//   //Hide spinner
-//   showSpinner()
-
-//   //1.5sec delay
-//   setTimeout(() => {
-//     hideSpinner()
-//   }, 1500)
-// })
-
-// //Show spinner function
-// const showSpinner = () => {
-//   document.getElementById('btnSubmitText').style.display = 'none'
-//   document.querySelector('.fa-spinner').style.display = 'block'
-// }
-
-// //Hide spinner function
-// const hideSpinner = () => {
-//   document.getElementById('btnSubmitText').style.display = 'block'
-//   document.querySelector('.fa-spinner').style.display = 'none'
-// }
-
 const btnSubmit = document.getElementById('btnSubmit')
 
 btnSubmit.addEventListener('click', (event) => {
